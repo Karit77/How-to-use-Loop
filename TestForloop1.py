3320e475c0a80ec36d7b01585f1ec8ab2e858590
Karit77 <108479347+Karit77@users.noreply.github.com>
2022-12-16 23:13:08 +0700

    การใช้ loop
    รวมการใช้ if,else,braek

---
+++ TestForloop1.py
@@ -0,0 +1,19 @@
+# forloop1.py
+
+# ลูปตามจำนวน
+
+for i in range(5):
+	print('Hello',i)
+
+print('----Loop in List----')
+friend = ['Karit','Jame','Yai','Lak','Nop']
+
+for f in friend:
+	print(f)
+	if  f == 'Jame':
+		print('Hello Mr.jame')
+	else:
+		print('Hello Guest')
+
+#if f == 'Lak':
+#break #กรณีการหยุด loop โดยใช้ break
