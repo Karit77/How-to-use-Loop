a031a51d3a02b63ac5015f25f8ed544440b75ef5
Karit77 <108479347+Karit77@users.noreply.github.com>
2022-12-23 22:05:41 +0700

    การใช้ลูปในรูปแบบของกำหนดค่า Dic

---
+++ TestLoopInDic.py
@@ -0,0 +1,13 @@
+#การใช้ลูปในรูปแบบของ Dictionary
+
+print('----Loop in Dictionary----')
+
+friend2 = {'Nine':'คุณนาย',
+            'Jame':'คุณเจม',
+            'Yai':'คุณใหญ่'}
+
+for f in friend2:
+	print(f)
+
+for f in friend2.items():
+    print(f)	
